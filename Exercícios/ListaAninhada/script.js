55a3aa0018b9eb627212d284343ab13f64af8ccb
Ariel Evangelista <43835997+GitArika@users.noreply.github.com>
2022-04-22 20:20:28 -0300

    feat: new styles & toggle list display (#1)

--- Exercícios/ListaAninhada/script.js
+++ Exercícios/ListaAninhada/script.js
@@ -1,42 +1,120 @@
-(function inicio() {
-    let projectButt = document.getElementById('projectList').style.display=''
-    let backEndBut = document.getElementById('backEndButton').style.display='none'
-    let appBut = document.getElementById('appButton').style.display= "none";
-    let configButt = document.getElementById('configButton').style.display='none';
-    let frontEndButt = document.getElementById('frontEndButton').style.display='none';
-    let appList = document.getElementById('appList').style.display='none';
-    let configList = document.getElementById('configList').style.display='none';
-    let publicList = document.getElementById('publicList').style.display='none';
-    let publicButt = document.getElementById('publicButton').style.display='none'
+(function() {
+    hideAll();
 })();
-function showProject(){
-    document.getElementById('backEndButton').style.display=''
+
+function hideAll() {
+    document.getElementById('backEndButton').style.display='none';
+    document.getElementById('appButton').style.display= "none";
+    document.getElementById('configButton').style.display='none';
+    document.getElementById('frontEndButton').style.display='none';
+    document.getElementById('appList').style.display='none';
+    document.getElementById('configList').style.display='none';
+    document.getElementById('publicList').style.display='none';
+    document.getElementById('frontendList').style.display='none';
+    document.getElementById('publicButton').style.display='none';
 }
-function showBackEndOptions(){
-    document.getElementById('appButton').style.display='';
-    document.getElementById('configButton').style.display='';
-    document.getElementById('frontEndButton').style.display='';
+
+/** 
+ * @param {*} htmlComponent  
+ * Check display property on htmlComponent and toggle visibility. 
+ */
+function toggleEfect(htmlComponent) {
+    if (htmlComponent.style.display == 'none' ) { 
+        htmlComponent.style.display = 'block';
+    } else {
+        htmlComponent.style.display = 'none';
+    };
 }
-function showAppList(){
-    document.getElementById('appList').style.display='';
+
+/**
+ * @param {*} htmlComponent  - toggle display of htmlComponent to block
+ */
+function hideEffect(htmlComponent) { 
+    htmlComponent.style.display = 'none';
 }
-function showConfigList() {
-    document.getElementById('configList').style.display='';
+
+function toggleProject(){
+    const backEndButton = document.getElementById('backEndButton');
+
+    toggleEfect(backEndButton)
+
+    // Hide Childrens
+    if(backEndButton.style.display == 'none') { 
+        const appButton = document.getElementById('appButton');
+        const configButton = document.getElementById('configButton');
+        const frontendButton = document.getElementById('frontEndButton');
+        const publicButton = document.getElementById('publicButton');
+        const appList = document.getElementById('appList');
+        const configList = document.getElementById('configList');
+        const frontendList = document.getElementById('frontendList');
+        const publicList = document.getElementById('publicList');
+
+        hideEffect(appList);
+        hideEffect(configList);
+        hideEffect(frontendList);
+        hideEffect(publicList);
+    
+        hideEffect(publicButton);
+        hideEffect(appButton);
+        hideEffect(configButton);
+        hideEffect(frontendButton);
+        hideEffect(frontendList);
+    }
 }
-function showFrontEndList() {
-    document.getElementById('publicButton').style.display=''
+
+function toggleBackEndOptions(){
+    const appButton = document.getElementById('appButton');
+    const configButton = document.getElementById('configButton');
+    const frontendButton = document.getElementById('frontEndButton');
+
+    toggleEfect(appButton);
+    toggleEfect(configButton);
+    toggleEfect(frontendButton);
+
+    // Hide Childrens
+    if(appButton.style.display == 'none') {
+        const appList = document.getElementById('appList');
+        const configList = document.getElementById('configList');
+        const frontendList = document.getElementById('frontendList');
+        const publicList = document.getElementById('publicList');
+        const publicButton = document.getElementById('publicButton');
+
+        hideEffect(appList);
+        hideEffect(configList);
+        hideEffect(frontendList);
+        hideEffect(publicList);
+        hideEffect(publicButton);
+    }
 }
-function showPublicList() {
-    document.getElementById('publicList').style.display='';
+
+function toggleAppList(){
+    const appList = document.getElementById('appList');
+
+    toggleEfect(appList);
 }
-function closeProject() {
-    let projectButt = document.getElementById('projectList').style.display=''
-    let backEndBut = document.getElementById('backEndButton').style.display='none'
-    let appBut = document.getElementById('appButton').style.display= "none";
-    let configButt = document.getElementById('configButton').style.display='none';
-    let frontEndButt = document.getElementById('frontEndButton').style.display='none';
-    let appList = document.getElementById('appList').style.display='none';
-    let configList = document.getElementById('configList').style.display='none';
-    let publicList = document.getElementById('publicList').style.display='none';
-    let publicButt = document.getElementById('publicButton').style.display='none'
+
+function toggleConfigList() {
+    const configList = document.getElementById('configList');
+
+    toggleEfect(configList);
+}
+
+function toggleFrontEndList() {
+    const frontendList = document.getElementById('frontendList');
+    const publicButton = document.getElementById('publicButton');
+
+    toggleEfect(frontendList);
+    toggleEfect(publicButton);
+    
+    if(publicButton.style.display == 'none') { 
+        const publicList = document.getElementById('publicList');
+        
+        hideEffect(publicList);
+    }
+}
+
+function togglePublicList() {
+    const publicList = document.getElementById('publicList');
+
+    toggleEfect(publicList);
 }
